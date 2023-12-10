4d53ac97f615c560240eeeae9351367084e1f985
yukihira_pot <r.magoshi.ma54@gmail.com>
2023-12-11 03:26:06 +0900

    不要なインポートを削除

--- src/components/switch/ToggleModeSwitch.tsx
+++ src/components/switch/ToggleModeSwitch.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material/styles";
-import Switch, { SwitchProps } from "@mui/material/Switch";
+import Switch from "@mui/material/Switch";
 
 const ToggleModeSwitch = styled(Switch)(({ theme }) => ({
   width: 62,
