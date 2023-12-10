3ff0231684ebb47af24e1e9f4ae2fc3589b699a3
yukihira_pot <r.magoshi.ma54@gmail.com>
2023-12-11 03:39:43 +0900

    デプロイ用に vite.config.ts を編集

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/amedos/",
+  build: {
+    outDir: "./dist",
+  },
   plugins: [react()],
-})
+});
